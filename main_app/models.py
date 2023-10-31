2b6094b4d335f5a060c6f5d26121f80d1c88486e
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-31 16:57:24 -0400

    create Article model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+  title = models.CharField(max_length=150)
+  content_main = models.TextField()  
+  content_section_1 = models.TextField(null=True, blank=True)  
+  content_section_2 = models.TextField(null=True, blank=True) 
+  likes = models.IntegerField(default=0)
+  # author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
+
