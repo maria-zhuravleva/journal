1a1a5450d2223d2fe13f0b54c66519330e1285a5
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-31 17:37:21 -0400

    create view function for article details page

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,7 @@ def about(request):
 def article_index(request):
   articles = Article.objects.all()
   return render(request, 'articles/index.html', { 'articles': articles })
+
+def article_detail(request, article_id):
+  article = Article.objects.get(id=article_id)
+  return render(request, 'articles/detail.html', { 'article': article })
