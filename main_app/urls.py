6bdde0bfd38a3d0e9087d35e46b3fb958a5f29b9
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-31 17:35:50 -0400

    add route for article details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('articles/', views.article_index, name='article-index'),
+  path('articles/<int:article_id>/', views.article_detail, name='article-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
+from .models import Article
 
-from django.http import HttpResponse
-
-class Article: 
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-articles = [
-  Article('Lolo', 'tabby', 'Kinda rude.', 3),
-  Article('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Article('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Article('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -23,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def article_index(request):
+  articles = Article.objects.all()
   return render(request, 'articles/index.html', { 'articles': articles })
