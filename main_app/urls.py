8a486728a33b8d6c5bcf0d72c8dc8a383d56066b
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-31 18:30:13 -0400

    create routes to delete and update article

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('articles/', views.article_index, name='article-index'),
   path('articles/<int:article_id>/', views.article_detail, name='article-detail'),
   path('articles/create/', views.ArticleCreate.as_view(), name='article-create'),
+  path('articles/<int:pk>/update/', views.ArticleUpdate.as_view(), name='article-update'),
+  path('articles/<int:pk>/delete/', views.ArticleDelete.as_view(), name='article-delete'),
 ]
