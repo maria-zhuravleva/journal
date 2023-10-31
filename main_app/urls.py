9df42d427239def79b55a0a38a85839c786aaabd
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-10-31 16:31:12 -0400

    define route to articles index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('articles/', views.article_index, name='article-index'),
 ]
