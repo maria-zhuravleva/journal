2693ebaafac26aa6d43cfbe27846753eae12aef3
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-01 12:09:21 -0400

    add sign up view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Article
 
 
@@ -33,3 +35,17 @@ class ArticleUpdate(UpdateView):
 class ArticleDelete(DeleteView):
   model = Article
   success_url = '/articles/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('article-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
