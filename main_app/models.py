6c6b1f2fe184a763e69ecd4bf461bc14a03f9296
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-03 11:19:49 -0400

    add created at field to Article model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Article(models.Model):
@@ -9,6 +10,7 @@ class Article(models.Model):
   content_section_2 = models.TextField(null=True, blank=True) 
   likes = models.IntegerField(default=0)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(default=timezone.now)
 
   def __str__(self):
     return self.title
