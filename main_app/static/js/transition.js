3f71ada381a736baa707f6dd5477a61587df1064
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-07 11:02:23 -0500

    add transition animation to home page

---
+++ main_app/static/js/transition.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('click', function(event) {
+    const anchor = event.target.closest('a');
+    if (anchor && (anchor.id === 'tapHere' || anchor.id === 'tapArrow')) {
+      event.preventDefault();
+      document.querySelector('.landingPage').classList.add('slide-out');
+      setTimeout(function() {
+        window.location.href = anchor.getAttribute('href');
+      }, 200);
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const images = document.querySelectorAll('.slide-in');
+
+  images.forEach(image => {
+    image.style.transform = 'translateX(0)';
+    image.style.opacity = '1';
+  });
+});
+
+
+
+
+
+
+
+
